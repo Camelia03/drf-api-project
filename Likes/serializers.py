346458d0436eccbbddf7057231773699a6469502
Camelia03 <camelia_weber@yahoo.com>
2023-09-27 12:41:19 +0000

    Like Serializer Handling Duplicate Likes / Integrity Errors

--- Likes/serializers.py
+++ Likes/serializers.py
@@ -15,7 +15,15 @@ class LikeSerializer(serializers.ModelSerializer):
             'id', 'created_at', 'owner', 'post'
         ]
 
-
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
+        
+        
 class CommentDetailSerializer(CommentSerializer):
     """
     Serializer for the Comment model used in Detail view
