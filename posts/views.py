a82418ae34ef7cbd2e0473daf5756410df249588
Camelia03 <camelia_weber@yahoo.com>
2023-09-28 11:56:41 +0000

    Adding the Search Feature to our API

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -17,8 +17,14 @@ class PostList(generics.ListCreateAPIView):
         comments_count= Count('comment', distinct=True)
         ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
     ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+
     ordering_fields = [
         'likes_count',
         'comments_count',
